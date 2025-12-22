52502847cf2ac100b331d0a75950df1ffd05900b
TheJetDev <thejetdev.contact@gmail.com>
2025-12-22 20:01:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,29 @@
 // sw.js
+const CACHE_NAME = 'simple-calc-app-v10'; // v10にアップ
 
-// キャッシュ名を更新（v7）
-
-const CACHE_NAME = 'simple-calc-app-v9';
-// 現在のファイル名に完全に一致させました
 const urlsToCache = [
   './',
   'index.html',
   'manifest.json',
   'apple-touch-icon.png',
-  'favicon-96x96.png',
   'web-app-manifest-192x192.png',
   'web-app-manifest-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
