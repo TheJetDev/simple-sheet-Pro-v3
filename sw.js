a97a4659559e24b4d55381c56f04c461be090cdb
TheJetDev <thejetdev.contact@gmail.com>
2025-12-29 14:22:26 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = 'simple-calc-app-v11'; 
+const CACHE_NAME = 'simple-calc-app-v12'; 
 
 const urlsToCache = [
   './',
@@ -28,3 +28,4 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
+
